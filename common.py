081736fcc80ff9c298a4a61174dd820da41e5592
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-15 15:47:10 +0530

    Create common.py

---
+++ common.py
@@ -0,0 +1,18 @@
+a=list(map(str,input().split()))
+n=len(a)
+c=[]
+for i in range(0,n):
+    c.append(len(a[i]))
+s=min(c)
+b=[]
+for k in range(0,n): 
+    for i in a[k]:
+        if(k<n-1):
+            for j in a[k+1]:
+                    if(i==j):
+                        b.append(i)
+b = list(dict.fromkeys(b))
+for i in b:
+    print(i,end="")
+    
+            
