b0da5653f20e4485c0880a30c655807e512e5724
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-15 16:12:00 +0530

    Create sai.py

---
+++ sai.py
@@ -0,0 +1,28 @@
+e=int(input())
+a=[]
+for i in range(e):
+    v=input()
+    a.append(v)
+n=len(a)
+c=[]
+for i in range(0,n):
+    c.append(len(a[i]))
+s=min(c)
+b=[]
+for k in range(0,n): 
+    for i in a[k]:
+        if(k<n-1):
+            for j in a[k+1]:
+                    if(i==j):
+                        b.append(i)
+def uniq(input):
+    output=[]
+    for x in input:
+        if x not in output:
+          output.append(x)
+    for i in output:
+        print(i,end="")
+uniq(b)
+
+    
+            
